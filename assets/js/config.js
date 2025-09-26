3f29b4f42c2fd140296c12d958c4d9fa8db5eb4c
FLYBONDINET <NICOLAS.EGUI@FLYBoNDI.COM>
2025-09-26 01:03:25 -0300

    fg

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,2 @@
-// URL de la App Web de Google Apps Script
-// Asegurate que sea la de tu despliegue (Deploy > New deployment > Web app)
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzxWWNbpuYBd--VcoSyr5cwQE7FvjtAE8ZFRVM_QEnBjc3hW7Lb9QWhJTWJS9G1cCr3/exec";
-
+// assets/js/config.js
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx2fTx90Mi4caagXHl3nezAVD8aL6PewU8nBQ3ZpmNJrpXzhK3rBR5vXKxom3nq3b6m/exec";
